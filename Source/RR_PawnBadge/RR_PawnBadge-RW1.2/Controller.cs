ec76fa6186d746adeca89fcdbf49efb343f7fc19
Ogliss <randomdragon2@hotmail.com>
2021-06-21 23:48:43 +0100

    fix for iTab conflict

---
+++ Source/RR_PawnBadge/RR_PawnBadge-RW1.2/Controller.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using UnityEngine;
+
+namespace RR_PawnBadge
+{
+	[StaticConstructorOnStartup]
+    public static class Controller
+    {
+		public static readonly Texture2D GreyTex = NewSolidColorTexture(Color.gray);
+
+		public static Texture2D NewSolidColorTexture(Color color)
+		{
+			if (!UnityData.IsInMainThread)
+			{
+				Log.Error("Tried to create a texture from a different thread.", false);
+				return null;
+			}
+			Texture2D texture2D = new Texture2D(35, 35);
+			texture2D.name = "RR_PawnBadge-SolidColorTex-" + color;
+
+			var fillColorArray = texture2D.GetPixels();
+			for (var i = 0; i < fillColorArray.Length; ++i)
+			{
+				fillColorArray[i] = color;
+			}
+			texture2D.SetPixels(fillColorArray);
+			texture2D.Apply();
+			return texture2D;
+		}
+
+		public static void EditDefs()
+		{
+			IEnumerable<ThingDef> things = (
+				from def in DefDatabase<ThingDef>.AllDefs
+				where def.race != null && def.race.Humanlike
+				select def
+			);
+			foreach (ThingDef t in things)
+			{
+				// add badge tab to pawns
+				if (t.inspectorTabsResolved == null)
+				{
+					t.inspectorTabsResolved = new List<InspectTabBase>(1);
+				}
+				t.inspectorTabs.Add(typeof(ITab_Pawn_Badge));
+
+			//	t.inspectorTabsResolved.Add(InspectTabManager.GetSharedInstance(typeof(ITab_Pawn_Badge)));
+				Log.Message("added Badge tab to " + t.defName);
+				// add Badge component property to all humanlike pawns
+				if (t.comps == null)
+				{
+					t.comps = new List<CompProperties>(1);
+				}
+				t.comps.Add(new CompProperties_Badge());
+				Log.Message("added Badge comp to " + t.defName);
+				t.ResolveReferences();
+			}
+		}
+
+		static Controller()
+		{
+			EditDefs();
+		}
+	}
+}
