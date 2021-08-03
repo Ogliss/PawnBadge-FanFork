d9be624a414d5a7cc99d14aea9adf4f616fe7761
Ogliss <randomdragon2@hotmail.com>
2021-08-03 07:41:39 +0100

    1

--- Source/RR_PawnBadge/scripts/ModCopy.bat
+++ Source/RR_PawnBadge/scripts/ModCopy.bat
@@ -1,3 +1,3 @@
 @echo off
-robocopy "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\PawnBadge-FanFork" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld1.2\Mods\PawnBadge-FanFork" /MIR /xd .git Source Ideology 1.3 /xf "*.zip" "*.7z" "*.psd" "*.psb" "*.md" ".gitattributes" ".gitignore" "About.xml" "LoadFolders.xml" "LICENSE"
+robocopy "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\PawnBadge-FanFork" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld1.2\Mods\PawnBadge-FanFork" /MIR /xd .git Source Ideology 1.3 /xf "*.zip" "*.7z" "*.psd" "*.psb" "*.md" ".gitattributes" ".gitignore" "About.xml" "LoadFolders.xml" "PublishedFileId.txt" "LICENSE"
 exit 0
